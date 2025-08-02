272375d472244db8b1b6ea3067480921093937fd
TooTired27 <mtshalipammiesnethemba@gmail.com>
2025-08-03 00:41:16 +0200

    Initail commit

---
+++ ClassifiedAd.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace TestClassifiedAd
+{
+   
+    internal class ClassifiedAd
+    {
+        string Catergory;
+        int numWords = 0;
+        double price = 0;
+        readonly double rate = 0.09;
+        public ClassifiedAd(string Category,int nWords)
+        {
+            this.Catergory = Category;
+            this.numWords = nWords;
+           price=rate*numWords;
+        }
+       public ClassifiedAd(string Category,int nWords,double newRate)
+        {
+            this.Catergory=Category;
+            this.numWords = nWords;
+            rate = newRate;
+            price=nWords*newRate;
+        }
+        public void SetCategory(string Category)
+        {
+            this.Catergory = Category;
+        }
+        public string GetCategory()
+        {
+            return this.Catergory;
+        }
+        public void SetWords(int nWords)
+        {
+            this.numWords = nWords;
+            price= nWords*0.09;
+
+        }
+        public int GetWords()
+        {
+            
+            return this.numWords;
+        }
+
+        public double GetPrice(double price)
+        {
+          return price;
+        }
+        public void DisplayAd()
+        {
+            WriteLine("Category: {0}, Number of words: {1}, Price: {2}.",Catergory,numWords,price);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+namespace TestClassifiedAd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            ClassifiedAd[] adList = new ClassifiedAd[5];
+            int nrEl = 0;
+
+            for (int x=nrEl; x< adList.Length; x++)
+            {
+
+                AddClassifiedAd(adList, ref nrEl);
+
+            }
+            WriteLine();
+            WriteLine("List of advertisements:");
+            for(int x = 0; x < nrEl; x++)
+            {
+                adList[x].DisplayAd();
+            }
+
+            ReadLine();
+        }
+        static void AddClassifiedAd(ClassifiedAd[]adList,ref int nrEl)
+        {
+            Write("Enter the category for the advertisement: ");
+            string cat = ReadLine();
+            Write("Enter the number of words in the advertisement: ");
+            int words = int.Parse(ReadLine());
+            Write("Does this customer have a different rate: (Y/N) ");
+            char response= char.Parse(ReadLine().ToUpper());
+            if (response == 'Y')
+            {
+                Write("Enter the rate for this customer: ");
+                double rate=double.Parse(ReadLine());
+                adList[nrEl] = new ClassifiedAd(cat, words, rate);
+                nrEl++;
+            }
+            else
+            {
+                adList[nrEl] = new ClassifiedAd(cat, words);
+                nrEl++;
+            }
+            WriteLine();
+        }
+    }
+}
